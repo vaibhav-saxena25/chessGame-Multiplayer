67f9a7bbabe8371e300cd04cdbd4782a8f286796
Vaibhav-Saxena <vs8631304@gmail.com>
2024-08-28 21:25:05 +0530

    all done
    not pushed node modules

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const { Chess } = require('chess.js');
+const path = require('path');
+const { title } = require('process');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = 'W';
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.render("index",{title:"Chess Game"});
+})
+
+io.on("connection",function(socket){
+    console.log("connected");
+    if(!players.white){
+        players.white = socket.id;
+        socket.emit("playerRole","w");
+    }else if(!players.black){
+        players.black = socket.id;
+        socket.emit("playerRole","b");
+    }else{
+        socket.emit("spectatorRole");
+    }
+    socket.on("disconnect",function(){
+        if(socket.id === players.white){
+            delete players.white;
+        }
+        else if(socket.id === players.black){
+            delete players.black;
+        }
+    })
+
+    socket.on("move",(move)=>{
+        try{
+            if(chess.turn() === "w" && socket.id !== players.white) return;
+            if(chess.turn() === "b" && socket.id !== players.black) return;
+            const result = chess.move(move);
+            if(result){
+                currentPlayer = chess.turn();
+                io.emit("move",move);
+                io.emit("boardState",chess.fen());
+            }else{
+                console.log("Invalid Move",move);
+                socket.emit("Invalid Move",move);
+            }
+        }catch(err){
+            console.log(err);
+            socket.emit("Invalid Move",move);
+        }
+    })
+});
+
+server.listen(3000,function(){
+    console.log("server is listening on the port 3000");
+});         
